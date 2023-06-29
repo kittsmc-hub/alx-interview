d1588e3ddfcebd57ac4ab660f8d5c41cf4d481e9
kitts <kittsmakokha@gmail.com>
2023-06-29 14:17:35 +0000

    pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
-"""a function that returns a list of lists of integers representing the pascalstriangle"""
+"""A script to determine pascal's triangle for any number"""
+
 
 def pascal_triangle(n):
-    r = [1]
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    triangle = []
 
+    # return (trianlgle if n <= 0)
+    if n <= 0:
+        return triangle
     for i in range(n):
-        print(r)
-        tr = [0] + r
-        r = r + [0]
-        r = [x+y for x,y in zip(r,tr)]
+        temp_list = []
 
-pascal(6)
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+    # print(triangle)
+    return triangle
